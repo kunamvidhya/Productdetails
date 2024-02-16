4f969b34e852811dfbd7cfb44c1b55aad292175e
KISHOREREDDY0712 <navakishorekunam@gmail.com>
2024-02-16 15:07:07 +0530

    Alldetails

---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Details
+{
+    public class Product
+    {
+        public string ProductCode { get; }
+        public string ProductName { get; }
+        public double Price { get; }
+        public string Brand { get; }
+        public DateTime ManufacturingDate { get; }
+        public DateTime ExpiryDate { get; }
+
+        public Product(string code, string name, double price, string brand, DateTime manufacturingDate, DateTime expiryDate)
+        {
+            ProductCode = code;
+            ProductName = name;
+            Price = price;
+            Brand = brand;
+            ManufacturingDate = manufacturingDate;
+            ExpiryDate = expiryDate;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Product_Details;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        SortedList<string, Product> productList = new SortedList<string, Product>();
+
+        // Add five products to the list
+        productList.Add("ABC123", new Product("ABC123", "Product1", 10.99, "Brand1", new DateTime(2022, 02, 16), new DateTime(2023, 02, 16)));
+        productList.Add("DEF456", new Product("DEF456", "Product2", 15.99, "Brand2", new DateTime(2022, 03, 16), new DateTime(2023, 03, 16)));
+        productList.Add("GHI789", new Product("GHI789", "Product3", 20.99, "Brand3", new DateTime(2022, 04, 16), new DateTime(2023, 04, 16)));
+        productList.Add("JKL012", new Product("JKL012", "Product4", 25.99, "Brand4", new DateTime(2022, 05, 16), new DateTime(2023, 05, 16)));
+        productList.Add("MNO345", new Product("MNO345", "Product5", 30.99, "Brand5", new DateTime(2022, 06, 16), new DateTime(2023, 06, 16)));
+
+        // Display product details
+        Console.WriteLine("MfDate\tExpDate\tIndex\tPName\tPrice");
+        int index = 1;
+        foreach (var product in productList)
+        {
+            Console.WriteLine($"{product.Value.ManufacturingDate:MM/dd/yyyy}\t{product.Value.ExpiryDate:MM/dd/yyyy}\t{index}\t{product.Value.ProductName}\t{product.Value.Price:C}");
+            index++;
+        }
+    }
+}
